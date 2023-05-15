d937de1f84769a10078b4e97a19907d219b542b7
FrancescoFerrari4H <francesco.ferrari.studenti@isii.it>
2023-05-16 00:15:33 +0200

    commit1

--- commander.js
+++ commander.js
@@ -2,101 +2,49 @@
     const axios = require('axios');
 
     program
-    .version('1.0.0')
-    .description('Descrizione del tuo bot.')
-    .option('-s, --standings <nome_lega>', 'Mostra la classifica per la lega(stagione corrente)')
-    .option('-b, --bookmakers', 'Mostra l\'elenco dei bookmaker disponibili')
-    .parse(process.argv);
-
-    if (program.standings) {
-        console.log(program.standings);
-    const leagueName = program.standings;
-        try {
-        const leagueId = GetIdLega(leagueName);
-        const table = getLeagueStandings(leagueId);
-        const options = {
-            method: 'GET',
-            url: `https://api-football-v1.p.rapidapi.com/v3/leagues?id=${leagueId}`,
-            headers: {
-            'X-RapidAPI-Key': '7ce5e3dd38msh7e61d1893740d9bp10b7cajsn22cfc2156b63',
-            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-            }
-        };
-        const response = axios.request(options);
-        const leagueLogoUrl = response.data.response[0].league.logo;
-        const message = `*Classifica di ${leagueName}*\n[Logo della lega](${leagueLogoUrl})\n\n${table}`;
-        bot.sendMessage(msg.chat.id, message, { parse_mode: 'Markdown' });
-        } catch (error) {
-        console.error(error);
-        bot.sendMessage(msg.chat.id, 'Errore durante il recupero della classifica.');
+    .description('Recupera il miglior stadio di una determinata nazione.')
+    .option('-n, --nazione <nome_nazione>', 'Il nome della nazione di cui recuperare il miglior stadio.')
+    .action(() => {
+        if (!program.opts().nazione) {
+        console.error('Devi specificare il nome della nazione.');
+        return;
         }
-    }
-
-    if (program.bookmakers) {
-    const options = {
+        const countryName = program.opts().nazione;
+        const options = {
         method: 'GET',
-        url: 'https://api-football-v1.p.rapidapi.com/v3/odds/bookmakers',
+        url: 'https://api-football-v1.p.rapidapi.com/v3/venues',
+        params: { country: countryName },
         headers: {
-        'X-RapidAPI-Key': '7ce5e3dd38msh7e61d1893740d9bp10b7cajsn22cfc2156b63',
-        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+            'X-RapidAPI-Key': '27992d3c24msh65ef48d0ff03eeap16a901jsna4563f8bab6f',
+            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
         }
-    };
-    
-    try {
-        const response =  axios.request(options);
-        const bookmakers = response.data.response;
-        const bookmakerNames = bookmakers.map((bookmaker) => bookmaker.name);
-        const bookmakerMessage = bookmakerNames.join('\n');
-        const message = `*Ecco l'elenco dei bookmaker disponibili:*\n\n${bookmakerMessage}`;
-        bot.sendMessage(msg.chat.id, message, { parse_mode: 'Markdown' });
-    } catch (error) {
-        console.error(error);
-    }
-    }
-    async function GetIdLega(leagueName) {
-        const options = {
-            method: 'GET',
-            url: 'https://api-football-v1.p.rapidapi.com/v3/leagues',
-            headers: {
-                'X-RapidAPI-Key': '7ce5e3dd38msh7e61d1893740d9bp10b7cajsn22cfc2156b63',
-                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-            },
-            params: {
-                name: leagueName
-            }
         };
-    
-        try {
-            const response =  axios.request(options);
-            const leagues = response.data.response;
-            if (leagues.length === 0) {
-                throw new Error(`Non ci sono leghe disponibili con il nome: ${leagueName}`);
+        axios.request(options)
+        .then((response) => {
+            const results = response.data.results;
+            if (results > 0) {
+            const venue = response.data.response[0];
+            const message = `*Il miglior stadio in ${countryName} è:*\n\n` +
+                `Nome: ${venue.name}\n` +
+                `Indirizzo: ${venue.address}\n` +
+                `Città: ${venue.city}\n` +
+                `Capacità: ${venue.capacity}\n` +
+                `Superficie: ${venue.surface}\n\n` +
+                `[Visualizza immagine](${venue.image})`;
+            console.log(message);
+            } else {
+            console.log(`Nessun stadio trovato per il paese ${countryName}`);
             }
-            const league = leagues[0];
-            console.log(league.league.id);
-            return league.league.id;
-        } catch (error) {
+        })
+        .catch((error) => {
             console.error(error);
-            throw new Error(`Si è verificato un errore durante la ricerca dell'ID della lega: ${error.message}`);
-        }
-    }
+            console.log('Si è verificato un errore durante la ricerca del miglior stadio.');
+        });
+    });
 
-    async function getLeagueStandings(leagueId, season = '2022') {
-        try {
-            const options = {
-                method: 'GET',
-                url: `https://api-football-v1.p.rapidapi.com/v3/standings?league=${leagueId}&season=${season}`,
-                headers: {
-                    'X-RapidAPI-Key': '7ce5e3dd38msh7e61d1893740d9bp10b7cajsn22cfc2156b63',
-                    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-                }
-            };
-            const response = axios.request(options);
-            const standings = response.data.response[0].league.standings[0];
-            const table = standings.map((team) => `${team.rank}. ${team.team.name} (${team.points} pts)`);
-            return table.join('\n');
-        } catch (error) {
-            console.error(error);
-            throw new Error('Errore durante il recupero della classifica.');
-        }
+    program.parse(process.argv);
+
+    if (program.args.length === 0) {
+    console.error('Devi specificare un comando.');
+    program.help();
     }
